8c670888a2fe238a6b2fdf4f99f7d8cfb54195ce
chemist-god <godtenkorang72@gmail.com>
2025-01-28 23:52:56 +0000

    event MessageUpdated

--- contracts/firstContract.sol
+++ contracts/firstContract.sol
@@ -1,11 +1,13 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract Inbox {
     string public message;
 
-    // Constructor
+    // Event to emit when the message is updated
+    event MessageUpdated(string newMessage);
+
+    // Constructor to initialize the message
     constructor(string memory initialMessage) {
         message = initialMessage;
     }
@@ -13,10 +15,6 @@ contract Inbox {
     // Function to set a new message
     function setMessage(string memory newMessage) public {
         message = newMessage;
+        emit MessageUpdated(newMessage); // Emit the event when the message is updated
     }
-
-    // The getMessage function is not needed since `message` is public
-    // function getMessage() public view returns (string memory) {
-    //     return message;
-    // }
 }
