80c1340291c36bf97ed9aee7af48e8e7a919c75f
chemist-god <godtenkorang72@gmail.com>
2025-03-20 23:48:52 +0000

    commit-solidity

--- contracts/coinSender.sol
+++ contracts/coinSender.sol
@@ -1,5 +1,5 @@
 
-// SPDX-License-Identifier: MIT
+ // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
 contract coinSender {
@@ -19,6 +19,18 @@ contract coinSender {
         require(msg.sender == minter);
         balances[reciever] += amount;
     }
+
+    error InsufficientBalance(uint requested,uint available);
+    // this error will help get the details of why an operation failed 
+    // it is returned to the caller function
+
     
+  // this allows anyone to send a coin to an account
+    function send(address receiver, uint amount) public {
+        require(amount <= balances[msg.sender], InsufficientBalance(amount, balances[msg.sender]));
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Sent(msg.sender, receiver, amount);
+    }
     
 }
