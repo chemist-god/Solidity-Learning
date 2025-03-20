7d59575d3e294ba66cbcdfffcfbef459a61b79db
chemist-god <godtenkorang72@gmail.com>
2025-03-20 23:54:24 +0000

    update-visibility

--- contracts/coinSender.sol
+++ contracts/coinSender.sol
@@ -1,36 +1,35 @@
-
- // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-contract coinSender {
+contract CoinSender {
     
     address public minter;
-    mapping (address => uint) public  balances;
+    mapping (address => uint) public balances;
 
-    event Sent(address from, address to, uint amount);
+    event Sent(address indexed from, address indexed to, uint amount);
+    event Minted(address indexed receiver, uint amount);
 
     constructor() {
-        minter = msg.sender;
+        minter = msg.sender; // The address that deploys the contract is the minter
     }
 
-// this send an amount of newly created coins to an account
-// and can only be called by the contructor creator
-    function mint(address reciever, uint amount) public  {
-        require(msg.sender == minter);
-        balances[reciever] += amount;
+    // This function mints an amount of newly created coins to an account
+    // and can only be called by the contract creator
+    function mint(address receiver, uint amount) external {
+        require(msg.sender == minter, "Only the minter can mint coins");
+        balances[receiver] += amount;
+        emit Minted(receiver, amount); // Emit an event for minting
     }
 
-    error InsufficientBalance(uint requested,uint available);
-    // this error will help get the details of why an operation failed 
-    // it is returned to the caller function
-
+    error InsufficientBalance(uint requested, uint available);
     
-  // this allows anyone to send a coin to an account
+    // This allows anyone to send coins to an account
     function send(address receiver, uint amount) public {
-        require(amount <= balances[msg.sender], InsufficientBalance(amount, balances[msg.sender]));
+        if (amount > balances[msg.sender]) {
+            revert InsufficientBalance(amount, balances[msg.sender]);
+        }
         balances[msg.sender] -= amount;
         balances[receiver] += amount;
         emit Sent(msg.sender, receiver, amount);
     }
-    
 }
