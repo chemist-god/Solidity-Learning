4072e308aa7b967ae220724c1189815ace238dc5
chemist-god <godtenkorang72@gmail.com>
2025-04-04 23:48:19 +0000

    commit- foodDelivery_2

---
+++ contracts/FoodDelivery_2.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract FoodDelivery {
+    //structs
+    struct Restaurant {
+        string name;
+        string location;
+        uint256 registrationTime;
+        bool isRegistered;
+    }
+}
