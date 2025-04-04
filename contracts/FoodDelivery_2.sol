3f4d3987b8aa5085966c0a9b2848d0a417c0ab3e
chemist-god <godtenkorang72@gmail.com>
2025-04-04 23:59:00 +0000

    Order-status

--- contracts/FoodDelivery_2.sol
+++ contracts/FoodDelivery_2.sol
@@ -9,9 +9,22 @@ contract FoodDelivery {
         uint256 registrationTime;
         bool isRegistered;
     }
+
+    // Restaurant menu items
     struct MenuItem{
         string name;
         uint256 price;
         bool isAvailable;
+    }
+
+    //Order
+    struct Order {
+        uint256 OrderId;
+        address customer;
+        uint256 totalAmount;
+        uint256[] itemsIds;
+        bool status;
+        address restaurant;
+        uint256 timestamp; 
     }
 }
