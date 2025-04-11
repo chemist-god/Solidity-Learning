c31e0bd6c88c2e0091b87c26b9d0c021f50697b6
chemist-god <godtenkorang72@gmail.com>
2025-04-11 23:45:14 +0000

    commit-events

--- contracts/eCommerce.sol
+++ contracts/eCommerce.sol
@@ -17,4 +17,15 @@ contract Ecommerce {
     // a counter for product IDs
     uint256 public productCount;
 
+            //DECLARING ALL MY EVENTS
+            // Event to emit when a product is added
+            //Event to emit when a product is purchased
+             /// Event to emit when a product is refunded
+
+
+    event ProductAdded(uint256 id, string name, uint256 price, address seller);
+
+    event ProductPurchased(uint256 id, address buyer);
+
+    event ProductRefunded(uint256 id, address buyer);
 }
