b95ec2d6e41795432c2220c141b3b586e24db17a
chemist-god <godtenkorang72@gmail.com>
2025-04-15 23:58:46 +0000

    commit-officialToken

---
+++ contracts/myOficialERC721.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Burnable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ChemistOfficialToken is ERC721, ERC721Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC721("chemistOfficialToken", "CHOTK")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+}
