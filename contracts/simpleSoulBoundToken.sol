6dcf77c2606e0c061939e00d06db2813206e3acf
chemist-god <godtenkorang72@gmail.com>
2025-04-17 23:52:53 +0000

    event-mint,burn

--- contracts/simpleSoulBoundToken.sol
+++ contracts/simpleSoulBoundToken.sol
@@ -5,5 +5,6 @@ contract SimpleSoulBoundToken {
     address public admin;
     mapping(address => uint256) public sbt;
 
-    
+    event Minted(address indexed to, uint256 tokenId);
+    event Burned(address indexed from, uint256 tokenId);
 }
