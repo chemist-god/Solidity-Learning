4538f8dd0a21430b1974f236ec94f80cc42ce56c
chemist-god <godtenkorang72@gmail.com>
2025-04-17 23:54:24 +0000

    mint-SBT

--- contracts/simpleSoulBoundToken.sol
+++ contracts/simpleSoulBoundToken.sol
@@ -7,4 +7,21 @@ contract SimpleSoulBoundToken {
 
     event Minted(address indexed to, uint256 tokenId);
     event Burned(address indexed from, uint256 tokenId);
+
+    // Set deployer as admin
+    constructor() {
+        admin = msg.sender;
+    }
+
+    // Mint new SBT to an address (admin only)
+    function mint(address to, uint256 tokenId) external {
+        require(msg.sender == admin, "Only admin");
+        require(to != address(0), "Zero address");
+        require(sbt[to] == 0, "Already has SBT");
+        
+        sbt[to] = tokenId;
+        emit Minted(to, tokenId);
+    }
+
+
 }
