b0e0c288ed34caea088e27553a8dd487e93cac54
chemist-god <godtenkorang72@gmail.com>
2025-04-23 23:59:02 +0000

    rockPaper

---
+++ contracts/rockPaper.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract RockPaperScissors {
+    enum Choice { None, Rock, Paper, Scissors }
+    enum GameStatus { Waiting, Committed, Revealed, Completed }
+    
+    
+}
