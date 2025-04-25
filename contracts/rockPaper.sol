29915c4c273144bb455d08f2ed49b602c224f6c3
chemist-god <godtenkorang72@gmail.com>
2025-04-25 23:57:02 +0000

    struct player

--- contracts/rockPaper.sol
+++ contracts/rockPaper.sol
@@ -3,18 +3,29 @@ pragma solidity ^0.8.0;
 
 contract RockPaperScissors {
     enum Choice { None, Rock, Paper, Scissors }
-    enum GameStatus { Waiting, Committed, Revealed, Completed }
+    // enum GameStatus { Waiting, Committed, Revealed, Completed }
     
-    struct Game {
-        address player1;
-        address player2;
-        bytes32 player1Commit;
-        Choice player1Choice;
-        Choice player2Choice;
-        uint256 betAmount;
-        GameStatus status;
-        address winner;
+    // struct Game {
+    //     address player1;
+    //     address player2;
+    //     bytes32 player1Commit;
+    //     Choice player1Choice;
+    //     Choice player2Choice;
+    //     uint256 betAmount;
+    //     GameStatus status;
+    //     address winner;
+    // }
+    
+     struct Player {
+        address playerAddress;
+        Choice choice;
+        bool hasPlayed;
     }
-    
+    Player public player1;
+    Player public player2;
+
+    address public owner;
+    bool public gameEnded;
+    address public winner;
     
 }
