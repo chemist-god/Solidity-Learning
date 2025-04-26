3673b7949ea1c11a409b0d4bcea38c00b75d9610
chemist-god <godtenkorang72@gmail.com>
2025-04-26 23:58:31 +0000

    counter

---
+++ contracts/Counter.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract Counter {
+    uint256 public count;
+
+    // Function to get the current count
+    function get() public view returns (uint256) {
+        return count;
+    }
+
+    
+}
