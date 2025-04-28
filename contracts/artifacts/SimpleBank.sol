c782aec03c2053d4df01608e87d2ac7335e03953
chemist-god <godtenkorang72@gmail.com>
2025-04-28 23:49:46 +0000

    SimpleBank

--- contracts/artifacts/SimpleBank.sol
+++ contracts/artifacts/SimpleBank.sol
@@ -19,5 +19,23 @@ contract SimpleBank {
         _;
     }
 
-    
+    function deposit() public payable {
+        totalBalance += msg.value;
+        emit Deposited(msg.sender, msg.value);
+    }
+
+    function withdraw(uint _amount) public onlyOwner {
+        require(totalBalance >= _amount, "Insufficient balance");
+        totalBalance -= _amount;
+        payable(msg.sender).transfer(_amount);
+        emit Withdrawn(msg.sender, _amount);
+    }
+
+    function getBalance() public view returns (uint) {
+        return totalBalance;
+    }
+
+    function calculateBonus(uint amount) public pure returns (uint) {
+        return amount * 10 / 100;
+    }
 }
