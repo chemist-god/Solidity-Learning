c2d2115258374e654591f97e4ae195e644b4fd4b
chemist-god <godtenkorang72@gmail.com>
2025-04-29 23:57:25 +0000

    football_age

---
+++ contracts/footballAge.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract FootballAge {
+    
+ 
+    struct Player {
+        string name;
+        uint birthYear;
+    }
+
+    mapping(address => Player) public players;
+
+    
+    }
+
+
+
