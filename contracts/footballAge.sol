569c7f512f14b4c323a9b6bc08370d579e1878eb
chemist-god <godtenkorang72@gmail.com>
2025-04-30 00:00:01 +0000

    getPlayer

--- contracts/footballAge.sol
+++ contracts/footballAge.sol
@@ -11,8 +11,15 @@ contract FootballAge {
 
     mapping(address => Player) public players;
 
-    
+    function setPlayer(string memory _name, uint _birthYear) public {
+        require(_birthYear > 1900 && _birthYear <= block.timestamp / 1 years, "Invalid birth year");
+        players[msg.sender] = Player(_name, _birthYear);
     }
 
+    function getPlayerAge(address _playerAddress) public view returns (uint) {
+        require(players[_playerAddress].birthYear != 0, "Player not found");
+        return (block.timestamp / 1 years) - players[_playerAddress].birthYear;
+    }
+}
 
 
