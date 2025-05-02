eded871747852dbc4b614bbd9bd7593e95ca2e53
chemist-god <godtenkorang72@gmail.com>
2025-05-02 23:58:36 +0000

    create todo

--- contracts/TodoCRUD.sol
+++ contracts/TodoCRUD.sol
@@ -16,5 +16,18 @@ contract TodoCRUD {
     event TaskUpdated(uint id, string content, bool completed);
     event TaskDeleted(uint id);
 
-    
+    // Create a new task
+    function createTask(string memory _content) public {
+        tasks[nextId] = Task(nextId, _content, false);
+        emit TaskCreated(nextId, _content);
+        nextId++;
+    }
+
+    // Read a task by id
+    function getTask(uint _id) public view returns (uint, string memory, bool) {
+        require(tasks[_id].id != 0, "Task not found");
+        Task storage task = tasks[_id];
+        return (task.id, task.content, task.completed);
+    }
+
 }
