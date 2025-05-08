644d2d687caf287b6b82c07df1c152ea13cc7f13
chemist-god <godtenkorang72@gmail.com>
2025-05-08 23:51:52 +0000

    lendingPool

--- contracts/SimpleLendingPool.sol
+++ contracts/SimpleLendingPool.sol
@@ -12,5 +12,27 @@ contract SimpleLendingPool {
         deposits[msg.sender] += msg.value;
     }
 
-    
+    // Borrow funds from the pool
+    function borrow(uint256 amount) external {
+        require(amount > 0, "Borrow amount must be greater than zero");
+        require(amount <= deposits[msg.sender] / 2, "Cannot borrow more than half of deposited amount");
+
+        loans[msg.sender] += amount;
+        payable(msg.sender).transfer(amount);
+    }
+
+    // Repay loan with interest
+    function repay() external payable {
+        require(loans[msg.sender] > 0, "No active loan to repay");
+        uint256 interest = (loans[msg.sender] * interestRate) / 100;
+        require(msg.value >= loans[msg.sender] + interest, "Insufficient amount to repay loan with interest");
+
+        loans[msg.sender] = 0;
+        deposits[msg.sender] += msg.value - (loans[msg.sender] + interest);
+    }
+
+    // Check balance
+    function getBalance() external view returns (uint256) {
+        return deposits[msg.sender];
+    }
 }
