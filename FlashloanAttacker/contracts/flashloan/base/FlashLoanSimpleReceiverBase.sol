946994576c3f27d4320448f6ceee42c6c6473261
chemist-god <godtenkorang72@gmail.com>
2025-05-08 23:44:44 +0000

    FlashloanAttacker

---
+++ FlashloanAttacker/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol
@@ -0,0 +1,36 @@
+/*
+    
+   ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██    ██████  ███████ ██    ██
+  ██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██      ██    ██
+  ██      ██    ██ ██    ██ █████   ██████  ██    ██ ██    ██ █████      ██   ██ █████   ██    ██
+  ██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██       ██  ██
+   ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██ ██ ██████  ███████   ████
+  
+  Find any smart contract, and build your project faster: https://www.cookbook.dev
+  Twitter: https://twitter.com/cookbook_dev
+  Discord: https://discord.gg/cookbookdev
+  
+  Find this contract on Cookbook: https://www.cookbook.dev/contracts/FlashloanAttacker?utm=code
+  */
+  
+  // SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.10;
+
+import {IFlashLoanSimpleReceiver} from '../interfaces/IFlashLoanSimpleReceiver.sol';
+import {IPoolAddressesProvider} from '../../interfaces/IPoolAddressesProvider.sol';
+import {IPool} from '../../interfaces/IPool.sol';
+
+/**
+ * @title FlashLoanSimpleReceiverBase
+ * @author Aave
+ * @notice Base contract to develop a flashloan-receiver contract.
+ */
+abstract contract FlashLoanSimpleReceiverBase is IFlashLoanSimpleReceiver {
+  IPoolAddressesProvider public immutable override ADDRESSES_PROVIDER;
+  IPool public immutable override POOL;
+
+  constructor(IPoolAddressesProvider provider) {
+    ADDRESSES_PROVIDER = provider;
+    POOL = IPool(provider.getPool());
+  }
+}
