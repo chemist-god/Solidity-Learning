85dd0cc0247c5b17d01770fbc7d462fdcda6068f
chemist-god <godtenkorang72@gmail.com>
2025-05-24 00:00:08 +0000

    mintable

--- contracts/concepts/mintToken.sol
+++ contracts/concepts/mintToken.sol
@@ -1,14 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/dynamic/IERC20Metadata.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MintableToken is ERC20, Ownable {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+contract MintableToken is IERC20Metadata, Ownable {
+    constructor() ERC20("Mint Token", "MT") {}
 
-    // Mint function: only contract owner can mint new tokens
-    function mint(address to, uint256 amount) external onlyOwner {
-        _mint(to, amount);
+    // Initialize total supply on deployment
+    function initializeSupply(uint256 initialAmount) external onlyOwner {
+        _totalSupply = initialAmount;
+        emit Transfer(address(0), msg.sender, initialAmount);
+    }
+
+    // Mint function: updates the `_totalSupply` and emits a transfer event.
+    function mint(address to, uint256 amount) external override onlyOwner returns (bool success){
+        require(amount > 0,"Minting less than one token is not allowed.");
+        
+        _mint(to,amount);
+
+        emit Transfer(msg.sender,to,amount);
+        return true;
     }
 }
