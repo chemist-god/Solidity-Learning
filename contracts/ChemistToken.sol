158f55d753b70b0e1154887b7a761fe4db622d57
chemist-god <godtenkorang72@gmail.com>
2025-05-31 22:30:05 +0000

    my--token

---
+++ contracts/ChemistToken.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.27;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ChemistToken is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+    constructor(address recipient, address initialOwner)
+        ERC20("ChemistToken", "CTK")
+        Ownable(initialOwner)
+        ERC20Permit("ChemistToken")
+    {
+        _mint(recipient, 100000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
