517ce07006a67b98ff4105c0ca54ed26c2276b6c
chemist-god <godtenkorang72@gmail.com>
2025-05-31 22:30:57 +0000

    Tokenized Agriculture funding

---
+++ contracts/concepts/AgriToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract TokenizedAgriFund {
+    string public name = "Agriculture Funding Token";
+    string public symbol = "AFT";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+    address public owner;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event FundReceived(address indexed investor, uint256 amount);
+
+    
+}
