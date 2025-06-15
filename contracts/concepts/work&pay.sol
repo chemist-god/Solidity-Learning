9d35bf00b6222cb37952791685c0fc62d34fe860
chemist-god <godtenkorang72@gmail.com>
2025-06-15 23:54:17 +0000

    commit functions

--- contracts/concepts/work&pay.sol
+++ contracts/concepts/work&pay.sol
@@ -21,5 +21,33 @@ contract WorkAndPayCarRental {
     event PaymentMade(uint carId, address driver, uint amount, uint remainingBalance);
     event OwnershipTransferred(uint carId, address newOwner);
 
-   
+    function registerCar(string memory _model, uint _salesPrice, uint _paymentFrequency) public {
+        carCount++;
+        cars[carCount] = Car(carCount, _model, _salesPrice, 0, _paymentFrequency, payable(msg.sender), payable(address(0)), false);
+        emit CarRegistered(carCount, _model, _salesPrice, msg.sender);
+    }
+
+    function assignDriver(uint _carId, address _driver) public {
+        require(cars[_carId].owner == msg.sender, "Only the owner can assign a driver");
+        require(cars[_carId].driver == address(0), "Driver already assigned");
+
+        cars[_carId].driver = payable(_driver);
+        emit DriverAssigned(_carId, _driver, cars[_carId].paymentFrequency);
+    }
+
+    function makePayment(uint _carId) public payable {
+        Car storage car = cars[_carId];
+        require(car.driver == msg.sender, "Only assigned driver can pay");
+        require(car.isCompleted == false, "Car already fully paid");
+
+        car.paidAmount += msg.value;
+        car.owner.transfer(msg.value);
+        emit PaymentMade(_carId, msg.sender, msg.value, car.salesPrice - car.paidAmount);
+
+        if (car.paidAmount >= car.salesPrice) {
+            car.isCompleted = true;
+            car.owner = car.driver; // Transfer ownership
+            emit OwnershipTransferred(_carId, car.driver);
+        }
+    }
 }
