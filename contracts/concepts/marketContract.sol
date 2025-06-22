9be1ffa0c40777f85eaae8cd6de98aec70b95153
chemist-god <godtenkorang72@gmail.com>
2025-06-22 23:40:06 +0000

    include functions

--- contracts/concepts/marketContract.sol
+++ contracts/concepts/marketContract.sol
@@ -58,5 +58,52 @@ contract CommunityMarket {
         nextSaleId = 1;
     }
 
-    
+    // Anyone can add a product for sale
+    function addProduct(string memory _name, uint _price) public {
+        uint productId = nextProductId++;
+        products[productId] = Product(productId, msg.sender, _name, _price, true);
+        sellerProducts[msg.sender].push(productId);
+        emit ProductAdded(productId, msg.sender, _name, _price);
+    }
+
+    // Anyone can buy a product by sending enough Ether and stating why they are buying
+    function purchaseProduct(uint _productId, string memory _purpose) public payable {
+        Product storage product = products[_productId];
+        require(product.seller != address(0), "Product does not exist.");
+        require(product.isAvailable, "Product is not for sale.");
+        require(msg.value >= product.price, "Not enough payment.");
+
+        // Pay the seller
+        payable(product.seller).transfer(msg.value);
+
+        // (If you want to make products unique, set isAvailable to false here)
+
+        uint saleId = nextSaleId++;
+        sales[saleId] = Sale(saleId, _productId, product.seller, msg.sender, msg.value, block.timestamp, _purpose);
+        buyerSales[msg.sender].push(saleId);
+
+        emit ProductPurchased(saleId, _productId, msg.sender, msg.value, _purpose);
+    }
+
+    // Get info about a product by its ID
+    function getProduct(uint _productId) public view returns (uint id, address seller, string memory name, uint price, bool isAvailable) {
+        Product storage product = products[_productId];
+        return (product.id, product.seller, product.name, product.price, product.isAvailable);
+    }
+
+    // Get info about a sale by its ID
+    function getSale(uint _saleId) public view returns (uint saleId, uint productId, address seller, address buyer, uint pricePaid, uint timestamp, string memory purpose) {
+        Sale storage sale = sales[_saleId];
+        return (sale.saleId, sale.productId, sale.seller, sale.buyer, sale.pricePaid, sale.timestamp, sale.purpose);
+    }
+
+    // Get all products a seller has listed
+    function getProductsBySeller(address _sellerAddress) public view returns (uint[] memory) {
+        return sellerProducts[_sellerAddress];
+    }
+
+    // Get all sales a buyer has made
+    function getSalesByBuyer(address _buyerAddress) public view returns (uint[] memory) {
+        return buyerSales[_buyerAddress];
+    }
 }
