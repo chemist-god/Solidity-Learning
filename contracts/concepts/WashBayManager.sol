bddab778f21014aa73bd649d1759e3b25b018e4d
chemist-god <godtenkorang72@gmail.com>
2025-07-05 23:44:16 +0000

    commit events, modifier, constructor

--- contracts/concepts/WashBayManager.sol
+++ contracts/concepts/WashBayManager.sol
@@ -18,5 +18,24 @@ contract WashBayManager {
     uint256 public serviceCount;
     mapping(uint256 => Service) public services;
 
-   
+    event ServiceRequested(uint256 serviceId, address indexed customer, ServiceType serviceType, uint256 fee);
+    event WorkerAssigned(uint256 serviceId, address indexed worker);
+    event ServiceCompleted(uint256 serviceId);
+    event ServiceCancelled(uint256 serviceId);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not authorized");
+        _;
+    }
+
+    modifier onlyCustomer(uint256 serviceId) {
+        require(msg.sender == services[serviceId].customer, "Not the customer");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    
 }
