8e4be019f984b9875e5b1205f9596e58324d5782
chemist-god <godtenkorang72@gmail.com>
2025-07-09 05:17:27 +0000

    function to creatElection

--- contracts/tVote/ElectionManager.sol
+++ contracts/tVote/ElectionManager.sol
@@ -27,5 +27,32 @@ contract ElectionManager {
         owner = msg.sender;
     }
 
-    
+    function createElection(
+        string memory _name,
+        string memory _description,
+        string memory _bannerUrl,
+        uint256 _startDate,
+        uint256 _endDate
+    ) public returns (bytes32) {
+        require(msg.sender == owner, "Only admin can create elections");
+        require(_startDate < _endDate, "Invalid timeline");
+
+        bytes32 electionId = keccak256(abi.encodePacked(_name, _startDate, msg.sender, block.timestamp));
+
+        require(!elections[electionId].exists, "Election already exists");
+
+        elections[electionId] = Election({
+            name: _name,
+            description: _description,
+            bannerUrl: _bannerUrl,
+            startDate: _startDate,
+            endDate: _endDate,
+            exists: true
+        });
+
+        emit ElectionCreated(electionId, _name, _startDate, _endDate, msg.sender);
+        return electionId;
+    }
+
+   
 }
