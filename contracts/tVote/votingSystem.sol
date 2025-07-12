7fb93f9b803fc16751d01ee8b463a1f6fce9f50a
chemist-god <godtenkorang72@gmail.com>
2025-07-12 23:59:45 +0000

    votes

--- contracts/tVote/votingSystem.sol
+++ contracts/tVote/votingSystem.sol
@@ -66,4 +66,66 @@ contract VotingSystem {
         emit CandidatesRegistered(electionId, _candidates);
     }
 
+    /**
+     * @dev Cast a vote for a candidate in a specific election
+     */
+    function vote(uint electionId, uint candidateIndex) external {
+        ElectionVotes storage ev = electionVotes[electionId];
+        require(candidateIndex < ev.candidateCount, "Invalid candidate index");
+        require(!ev.hasVoted[msg.sender], "You already voted in this election");
+
+        // Get election info from ElectionFactory
+        (bool success, bytes memory data) = electionFactory.staticcall(
+            abi.encodePacked(
+                bytes4(keccak256("isElectionActive(uint256)")),
+                abi.encode(electionId)
+            )
+        );
+        require(success, "Call to ElectionFactory failed");
+        require(abi.decode(data, (bool)), "Election is not active");
+
+        // Mark voter as having voted
+        ev.hasVoted[msg.sender] = true;
+
+        // Record vote
+        ev.voteCount[candidateIndex] += 1;
+        ev.totalVotes += 1;
+
+        emit Voted(electionId, msg.sender, candidateIndex);
+    }
+
+    /**
+     * @dev Get number of votes a candidate received in an election
+     */
+    function getVoteCount(uint electionId, uint candidateIndex) external view returns (uint) {
+        return electionVotes[electionId].voteCount[candidateIndex];
+    }
+
+    /**
+     * @dev Check if a user has already voted in an election
+     */
+    function hasVoted(uint electionId, address voter) external view returns (bool) {
+        return electionVotes[electionId].hasVoted[voter];
+    }
+
+    /**
+     * @dev Get total votes cast in an election
+     */
+    function getTotalVotes(uint electionId) external view returns (uint) {
+        return electionVotes[electionId].totalVotes;
+    }
+
+    /**
+     * @dev Get list of candidate names for an election
+     */
+    function getCandidates(uint electionId) external view returns (string[] memory) {
+        return candidatesByElection[electionId];
+    }
+
+    /**
+     * @dev Get total number of candidates in an election
+     */
+    function getCandidateCount(uint electionId) external view returns (uint) {
+        return electionVotes[electionId].candidateCount;
+    }
 }
